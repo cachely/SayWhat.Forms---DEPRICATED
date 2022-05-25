fafcc58c6cd7c2ef26d1713fdc55f2038ec00b56
Ashley Proctor <lamoose@gmail.com>
2022-05-25 16:39:43 -0400

    adds a demo project to the repo

---
+++ SayWhat.Forms.Demo/SayWhat.Forms.Demo/SayWhat.Forms.Demo/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace SayWhat.Forms.Demo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
