9b0ae70ad8e7adde2a4bbec5ee1515d847b9e1d1
Ashley Proctor <ashleyproctordev@gmail.com>
2022-05-25 20:19:20 -0400

    adds project reference back to children projects

---
+++ SayWhat.Forms.Demo/SayWhat.Forms.Demo/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Xamarin.Forms;
+using System.Reflection;
+
+namespace SayWhat.Forms.Demo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //Initialization
+            Utilities.SayWhat.Settings.Initialize(Assembly.GetAssembly(typeof(MainPage)), "en-US");
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
